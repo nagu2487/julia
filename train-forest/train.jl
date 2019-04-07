55917eb949f82690662b0856782ca7791336e940
Balakrishnan <331017@1000100001180m.home>
2019-04-07 14:34:58 +1000

    Changed the Random Forest model

--- train-forest/train.jl
+++ train-forest/train.jl
@@ -1,16 +1,21 @@
 using DataFrames
 using DecisionTree
 using JLD
+using Random
 
 # Read the iris data set.
 df = readtable(ARGS[1], header = false)
 
 # Get the features and labels.
 features = convert(Array, df[:, 1:4])
+features = float.(features)
+
+
 labels = convert(Array, df[:, 5])
+labels = string.(labels)
 
 # Train decision tree classifier.
-model = RandomForestClassifier(ntrees=3, partialsampling=0.7)
+model = RandomForestClassifier(n_trees=3, partial_sampling=0.7)
 DecisionTree.fit!(model, features, labels)
 
 # Save the model.
